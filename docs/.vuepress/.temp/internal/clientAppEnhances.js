cfd68b42fbbe3854a59aa90adfa21de01885e76f
“F-one-1” <fy1033409628@163.com>
2021-11-11 15:09:15 +0800

    start a new version about Vue3 change.because before is Not OK

--- docs/.vuepress/.temp/internal/clientAppEnhances.js
+++ docs/.vuepress/.temp/internal/clientAppEnhances.js
@@ -1,7 +1,7 @@
-import clientAppEnhance0 from 'D:/vscode/Vue/ddd/node_modules/@vuepress/theme-default/lib/client/clientAppEnhance.js'
-import clientAppEnhance1 from 'D:/vscode/Vue/ddd/node_modules/@vuepress/plugin-medium-zoom/lib/client/clientAppEnhance.js'
-import clientAppEnhance2 from 'D:/vscode/Vue/ddd/node_modules/@vuepress/plugin-theme-data/lib/client/clientAppEnhance.js'
-import clientAppEnhance3 from 'D:/vscode/Vue/ddd/docs/.vuepress/clientAppEnhance.js'
+import clientAppEnhance0 from 'D:/vscode/Vue/elementplus-magic/node_modules/@vuepress/theme-default/lib/client/clientAppEnhance.js'
+import clientAppEnhance1 from 'D:/vscode/Vue/elementplus-magic/node_modules/@vuepress/plugin-medium-zoom/lib/client/clientAppEnhance.js'
+import clientAppEnhance2 from 'D:/vscode/Vue/elementplus-magic/node_modules/@vuepress/plugin-theme-data/lib/client/clientAppEnhance.js'
+import clientAppEnhance3 from 'D:/vscode/Vue/elementplus-magic/docs/.vuepress/clientAppEnhance.js'
 
 export const clientAppEnhances = [
   clientAppEnhance0,
--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -1,4 +1,4 @@
-import clientAppRootComponent0 from 'D:/vscode/Vue/ddd/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+import clientAppRootComponent0 from 'D:/vscode/Vue/elementplus-magic/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
 
 export const clientAppRootComponents = [
   clientAppRootComponent0,
