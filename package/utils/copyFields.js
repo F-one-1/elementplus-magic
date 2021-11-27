f87988c7ec7169641b52c850fc9b4506ba41576c
“F-one-1” <fy1033409628@163.com>
2021-11-27 14:00:54 +0800

    delete some package

--- package/utils/copyFields.js
+++
@@ -1,8 +0,0 @@
-import cloneDeep from 'lodash/cloneDeep'
-import unset from 'lodash/unset'
-
-export const copyFields = (obj = {}, exclude = []) => {
-  const result = cloneDeep(obj)
-  exclude.forEach(path => unset(result, path))
-  return result
-}
--- package/utils/fileUtils.js
+++
@@ -1,9 +0,0 @@
-export const fileUtils = {
-  ext (name) {
-    return name.replace(/.+\./, '')
-  },
-  name (name) {
-    const reg = new RegExp(`.${ this.ext(name) }$`)
-    return name.replace(reg, '')
-  }
-}
