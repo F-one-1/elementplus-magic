93971f43b47f5b22d9943f298f4bfe6d59db2fe6
“F-one-1” <fy1033409628@163.com>
2021-11-28 22:16:47 +0800

    add

--- docs/.vuepress/.temp/internal/pagesData.js
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -5,10 +5,10 @@ export const pagesData = {
   "v-cf0f0232": () => import(/* webpackChunkName: "v-cf0f0232" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/Buttons/index.html.js").then(({ data }) => data),
   // path: /demo/
   "v-1473bf53": () => import(/* webpackChunkName: "v-1473bf53" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/demo/index.html.js").then(({ data }) => data),
-  // path: /Form/
-  "v-12d19c72": () => import(/* webpackChunkName: "v-12d19c72" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/Form/index.html.js").then(({ data }) => data),
   // path: /Drawer/
   "v-3f443025": () => import(/* webpackChunkName: "v-3f443025" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/Drawer/index.html.js").then(({ data }) => data),
+  // path: /Form/
+  "v-12d19c72": () => import(/* webpackChunkName: "v-12d19c72" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/Form/index.html.js").then(({ data }) => data),
   // path: /Layer/
   "v-50dcd557": () => import(/* webpackChunkName: "v-50dcd557" */"D:/vscode/Vue/ddd/docs/.vuepress/.temp/pages/Layer/index.html.js").then(({ data }) => data),
   // path: /start/
